103f4003b2701d8a16c1986a0e27351c6c553ecf
Lawrence Milne <lmilne01@uoguelph.ca>
2019-12-06 23:50:47 -0500

    basics file created now to start styling

---
+++ main.js
@@ -0,0 +1,26 @@
+/*
+Author: Lawrence Milne
+gitHandle: qawse3dr
+*/
+
+
+const { app, BrowserWindow } = require('electron')
+
+
+var win = null;
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
