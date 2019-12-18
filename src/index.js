41649cbe403aeac01f7caceaeb132f1388732b2d
Lawrence Milne <lmilne01@uoguelph.ca>
2019-12-18 16:40:12 -0500

    added working volume bar still needs styling adding metronome next

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ const playBtn = document.getElementById("Play");
 const midiParser = require("midi-parser-js");
 const fs = require("fs");
 
+const volumeSlider = document.getElementById("volume");
 //sets the target to the speakers.
-var synth = new tone.Synth().toMaster();
+var synth = null;
+createSynth();
 
 var isPlaying = false;
 var song = {
@@ -26,30 +28,77 @@ var song = {
                   {name:"D#4",length:"16n"},{name:"F4",length:"16n"},{name:"F#4",length:"6n"},{name:"F4",length:"8n"},{name:"F#4",length:"8n"},{name:"G#4",length:"6n"},{name:"F#4",length:"8n"}
                   ,{name:"G#4",length:"6n"},{name:"A#4",length:"8n"},{name:"A#4",length:"4n"},{name:"F#4",length:"4n"}]
 };
+
+
 var canvas = document.getElementById("song-canvas"); //render
 var ctx = canvas.getContext("2d");
+var height = 550; //length and width of canvas
+var width = 750;
+
+//music vars
 var BPM = 120; //The beats per minute of song
+var volume = 0;
 
 //sets the rendering item for canvas
 setInterval(render,16);
+
+//Button Listener for playing.
 playBtn.addEventListener("click",play);
 
 
+/*********************GRAPHICS************************/
+/**Renders the sheetMusic*/
 function render(){ //displays the song canvas.
-  ctx.fillStyle = "#d3d3d3";
-  ctx.fillRect(0,0,750,550);
-  ctx.font = "12px Arial";
+
+  //Paints Screen
+  fillScreen("#d3d3d3")
+  /**draws BPM on canvas.*/
+  renderBPM();
+
+
+  /**draws music*/
+  drawMusic();
+}
+
+/**Resizes the canvas*/
+function resizeCanvas(newWidth,newHeight){
+  if(newWidth != 0){
+    width = newWidth;
+    canvas.width = width;
+  }
+  if(newHeight != 0){
+    height = newHeight;
+    canvas.height = height;
+  }
+  //canvas.parentElement.scrollTo(0,0);
+}
+/**fills the screen with colour colour*/
+function fillScreen(colour){
+  ctx.fillStyle = colour; //fill screen
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+
+}
+
+/**Draws the BPM.*/
+function renderBPM(){
+  ctx.font = "12px Arial";   //default font.
   ctx.fillStyle = "black";
   ctx.textAlign = "center";
   ctx.fillText("BPM: " + getBPM(),700,20);
 }
-/*drawcanvas*/
 
+/**Draws the sheet music*/
+function drawMusic(){
+
+}
+/*********************GRAPHICS************************/
 
 function play(event){ //plays the song or stops it.
+  resizeCanvas(6000,6000)
   if(!isPlaying){
     isPlaying = true;
     playBtn.innerText = "Stop"
+    //TODO: next add it so many tracks can be played
     let delta = tone.now(); //time passed
     for(note in song.notes){ //adds song to queue
       synth.triggerAttackRelease(song.notes[note].name,song.notes[note].length,delta);
@@ -60,12 +109,16 @@ function play(event){ //plays the song or stops it.
   } else{ //shuts off song. need to add in shut off at end of song.
     isPlaying = false;
     playBtn.innerText = "Play"
-    synth.dispose();
-     synth = new tone.Synth().toMaster();
+    createSynth();
 
   }
 }
-
+/*********MUSIC FUNCTIONS*************/
+/*Creates the synth object*/
+function createSynth(){
+  if(synth != null) synth.dispose()
+  synth = new tone.Synth().toMaster();
+}
 /*Gets the BPM*/
 function getBPM(){
   return BPM;
@@ -76,45 +129,54 @@ function setBPM(bpm){
   tone.Transport.bpm.value=BPM;
 }
 
+volumeSlider.oninput = () => {
+  volume = volumeSlider.value;
+  console.log(volume);
+  synth.volume.value = volume;
+}
+/*********MUSIC FUNCTIONS*************/
 
 function edit(){
 
 }
+
+/******************FILE IO************************/
+/*saves the current song to a song file using json*/
 function saveFile(fileName){
-  let data = JSON.stringify(song);
+  let data = JSON.stringify(song); //Turns objects into strings.
   fs.writeFile(fileName,data,(err) => {
-    if(err){
-      //popup
+    if(err){ //cretes error popup
+      electron.remote.dialog.showErrorBox("Invalid File",
+          err + "Please select a valid file");
       throw err;
     }
-    console.log("writen");
+
   })
 }
+/**Loads file from file fileName
+parse it from a json file.
+*/
 function loadFile(fileName){
   fs.readFile(fileName, (err,data) =>{
     if(err){
-      //do popup here
+      //creates popup here
+      electron.remote.dialog.showErrorBox("Invalid File",
+          err + " Please select a valid file");
       throw err;
     }
+    //loads the song
     song = JSON.parse(data);
     setBPM(song.bpm);
     setTitle(song.title);
 
   })
 }
-
+/******************FILE IO************************/
 function setTitle(title){
   let titleText = document.getElementById("title");
   titleText.innerText = title
 }
 
-/**SLEEP FUNCTIONS FROM
-https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
-AUTHOR: Dan Dascalescu
-*/
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 /********************IPC COMMUNICATIONS*************************/
 ipcRenderer.on("send-bpm", (event,bpm) => { //changes bpm to new given bpm
@@ -133,3 +195,4 @@ ipcRenderer.on("save-file", (event,fileName) => {
   console.log(fileName)
   console.log("saved")
 })
+/********************IPC COMMUNICATIONS*************************/
