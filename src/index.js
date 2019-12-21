c79c9edb6e45dd22966c4166518add1aae873fe6
Lawrence Milne <lmilne01@uoguelph.ca>
2019-12-20 21:52:05 -0500

    button now change colour when active. also metronome solo and count in are all working. note metronome goes for too long need to find right equation for finding how many times to play it

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 
 
-
+//includes
 const electron = require("electron");
 const path = require("path");
 const ipcRenderer = electron.ipcRenderer;
 const tone = require("tone");
-const playBtn = document.getElementById("play-btn");
 const fs = require("fs");
 
-const volumeSlider = document.getElementById("volume");
+//UI Elements
+
+//buttons
+const playBtn = document.getElementById("play-btn");
+const countBtn = document.getElementById("count-btn");
+const metronomeBtn = document.getElementById("metronome-btn");
+const soloBtn = document.getElementById("solo-btn");
+
+const volumeSlider = document.getElementById("volume"); //slider
+const canvas = document.getElementById("song-canvas"); //render
+
+//Button Listener for playing.
+playBtn.addEventListener("click",play);
+metronomeBtn.addEventListener("click",metronomeToggle);
+countBtn.addEventListener("click",countToggle);
+soloBtn.addEventListener("click",soloToggle);
+
+
+//Setting up graphics
+var ctx = canvas.getContext("2d");
+var height = canvas.height; //length and width of canvas
+var width = canvas.width;
+
+
 //sets the target to the speakers.
 var synth = null;
 createSynth();
@@ -45,24 +67,20 @@ var song = {
 };
 
 
-var canvas = document.getElementById("song-canvas"); //render
-var ctx = canvas.getContext("2d");
-var height = 550; //length and width of canvas
-var width = 750;
+
 
 //music vars
 var BPM = 120; //The beats per minute of song
 var volume = 0;
 var isPlaying = false;
-var isCountOn = true; //if starting count is on.
+var isCountOn = false; //if starting count is on.
 var metronome = false; //if the metronome is on.
 var isSolo = false; //if the instrument is soloing
 var currentInstrument = 0;
 //sets the rendering item for canvas
 setInterval(render,16);
 
-//Button Listener for playing.
-playBtn.addEventListener("click",play);
+
 
 
 /*********************GRAPHICS************************/
@@ -140,6 +158,8 @@ function setBPM(bpm){
   tone.Transport.bpm.value=BPM;
 }
 
+
+/*************BUTTON onclicks*********************/
 volumeSlider.oninput = () => {
   volume = volumeSlider.value;
   console.log(volume);
@@ -149,13 +169,14 @@ volumeSlider.oninput = () => {
 function playSong(){
   isPlaying = true;
   playBtn.innerText = "Stop"
-  let startOffset = (isCountOn) ? tone.Time("4n")*8:0;
+  let startOffset = (isCountOn) ? tone.Time("4n")*8 : 0; //offset for start count
+  let maxDelta = 0; //gets the length of the song.
   console.log(startOffset)
   synth.sync(); //start syncing the tracks
 
   if(isCountOn){
     for(let i = 0; i < 8; i++)
-    synth.triggerAttackRelease("C2","4n",tone.Time("4n")*i,1);
+    synth.triggerAttackRelease("C2","32n",tone.Time("4n")*i,1);
   }
   //loops through and syncs tracks
   for(track in song.tracks){
@@ -166,14 +187,24 @@ function playSong(){
     }
     let delta = startOffset; //time passed
     for(note in song.tracks[track].notes){ //adds song to queue
-      synth.triggerAttackRelease(song.tracks[track].notes[note].name,song.tracks[track].notes[note].length,delta);
-      delta += tone.TransportTime(song.tracks[track].notes[note].length); //add offset
+      synth.triggerAttackRelease(song.tracks[track].notes[note].name,song.tracks[track].notes[note].length,delta,0.5);
+      delta += tone.Time(song.tracks[track].notes[note].length); //add offset
       //console.log(song.tracks[track].notes[note]);
+      if(maxDelta<delta) maxDelta = delta;
     }
+
+    }
+    if(metronome){ //metronome is enabled
+      let delta = startOffset;
+      for(let ticks = 0; ticks < getTimeInBeats(maxDelta); ticks ++){ //converts time to quarter notes
+        synth.triggerAttackRelease("C2","32n",delta,1);
+        delta += tone.Time("4n"); //add offset
+      }
   }
-  tone.Transport.start(tone.now());
+  tone.Transport.start();
 }
 
+/**stops the notes from playing*/
 function stopPlay(){
   isPlaying = false;
   playBtn.innerText = "Play"
@@ -181,11 +212,45 @@ function stopPlay(){
   synth.unsync();
   createSynth()
 }
+
+/*Converts the time into how many beats it will take.*/
+function getTimeInBeats(time){
+  return time/60*BPM;
+}
+
+/*toggle the boolean metronome  also changing colour of button*/
+function metronomeToggle(){
+  metronome = (metronome) ? false : true;
+  if(metronome){
+    metronomeBtn.style.background="#4b81a6"
+  } else {
+    metronomeBtn.style.background="lavender";
+  }
+}
+
+/*toggles the solo boolean  also changing colour of button*/
+function soloToggle(){
+  isSolo = (isSolo) ? false : true;
+  if(isSolo){
+    soloBtn.style.background="#4b81a6"
+  } else {
+    soloBtn.style.background="lavender";
+  }
+}
+
+/*toggle the count boolean also changing colour of button*/
+function countToggle(){
+  isCountOn = (isCountOn) ? false : true;
+  if(isCountOn){
+    countBtn.style.background="#4b81a6"
+  } else {
+    countBtn.style.background="lavender";
+  }
+}
+
 /*********MUSIC FUNCTIONS*************/
 
-function edit(){
 
-}
 
 /******************FILE IO************************/
 /*saves the current song to a song file using json*/
